e8c2bd1c9bc4846cb766d1ecf08362d2f7529994
khaintph16089 <khaintph16089@fpt.edu.vn>
2022-03-12 15:57:07 +0700

    thêm sửa xóa

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,26 +1,64 @@
 
 //fake api
-const data = [
-    { id: 1, name: "product A" },
-    { id: 2, name: "product B" },
-    { id: 3, name: "product C" },
-    { id: 4, name: "product D" }
-];
+// const data = [
+//     { id: 1, name: "product A" },
+//     { id: 2, name: "product B" },
+//     { id: 3, name: "product C" },
+//     { id: 4, name: "product D" }
+// ];
+import mongoose from "mongoose";
 
-export const list = (req, res) => {
-    res.json(data);
+const Product = mongoose.model('Product', { name: String });
+
+export const list = async (req, res) => {
+    try {
+        const products = await Product.find({}).exec()
+        res.json(products);
+    } catch (error) {
+        res.status(400).json({
+            error: "không tìm thấy sản phẩm"
+        })
+    }
 }
-export const create = (req, res) => {
-    data.push(req.body);
-    res.json(data);
+export const create = async(req, res) => {
+    try {
+        const product = await new Product(req.body).save()
+        res.json();
+    } catch (error) {
+        res.status(400).json({
+            error: "không thêm được sản phẩm"
+        })
+    }
 }
-export const get = (req, res) => {
-    res.json(data.find(item => item.id == req.params.id));
+export const get = async (req, res) => {
+    try {
+        const product = await  Product.findOne({_id: req.params.id}).exec()
+        res.json(product);
+    } catch (error) {
+        res.status(400).json({
+            error: "không thêm được sản phẩm"
+        })
+    }
 }
-export const remove = (req, res) => {
-    res.json(data.filter(item => item.id != req.params.id));
+export const remove = async (req, res) => {
+    try {
+        const product = await Product.findOneAndDelete({_id: req.params.id}).exec()
+        res.json(product);
+    } catch (error) {
+        res.status(400).json({
+            error: "không thêm được sản phẩm"
+        })
+    }
 }
-export const update = (req, res) => {
-    const result = data.map(item => item.id == req.params.id ? req.body : item );
-    res.json(result);
+export const update = async (req, res) => {
+    const conditions = {_id: req.params.id,};
+    const update = ({name: 'product A update'})
+    try {
+        const product = await Product.findOneAndUpdate(conditions, update).exec()
+        res.json(product);
+    } catch (error) {
+        res.status(400).json({
+            error: "không thêm được sản phẩm"
+        })
+    }
 }
