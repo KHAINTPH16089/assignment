4fa9b470a25b5c21c23592d690d8fd5652f34689
khaintph16089 <khaintph16089@fpt.edu.vn>
2022-03-12 14:34:00 +0700

    update mongodb

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import express from "express";
 import cors from "cors";
 import product from "./routes/products";
 import morgan from "morgan";
+import mongoose from "mongoose";
 
 
 const app = express();
@@ -22,6 +23,10 @@ app.get('/user', (req, res) => {
     res.send("<h1>Home Page</h1>");
 })
 
+//mongoose db
+mongoose.connect("mongodb://localhost:27017/we16310")
+    .then(()=>{ console.log("kết nối db thành công"); })
+    .catch( error => {console.log(error);} )
 //connet
 const POST = 3001;
 app.listen(POST, () => {
